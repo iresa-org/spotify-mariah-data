fdc661f8847c424744dbf44c729a94439b03b593
kxbui <khoixbui@gmail.com>
2026-06-18 14:48:10 -0400

    annotate album names

--- scripts/config/album.config.ts
+++ scripts/config/album.config.ts
@@ -1,24 +1,21 @@
 export const SELECTED_ALBUMS = [
-  'spotify:album:0O4U8aVbBcWrq38ax0T6AO',
-  'spotify:album:0SHpIbyBLUugMXsl3yNkUz',
-  'spotify:album:0cS9prZ8u3fdbc7lmtCaMV',
-  'spotify:album:0v1DRRYBXYg1uVN1CIsyy0',
-  'spotify:album:16rTsMjlDt6DEbLRtxvcWu',
-  'spotify:album:1GzBLltCCKsiIJM3T3AWj3',
-  'spotify:album:1ibYM4abQtSVQFQWvDSo4J',
-  'spotify:album:2VtWkFLhMJAFsWkHAXwosS',
-  'spotify:album:2hHFZLYnwsYOOxTCrlNvg0',
-  'spotify:album:2tVxdAJ16NY9bpjb1h5fQc',
-  'spotify:album:31MluXLYC0ZnCSfUZ5T4GX',
-  'spotify:album:3RPImDZ7Ihh5YR5iJh1gH1',
-  'spotify:album:3VOqo81Nwyx8rcZEc2l379',
-  'spotify:album:6fC6BRXbuHSVobwWcwe6M7',
-  'spotify:album:5SwNGsGw1I8H361DKiYnnn',
-  'spotify:album:5VfesyhwiNgpEEPXlO5c84',
-  'spotify:album:60uzoZyNIXRVhhxTMHxe7P',
-  'spotify:album:6MljmKZLh52AUR1v5WpWst',
-  'spotify:album:6aouVhqJ9SkEUS2gAR0xBS',
-  'spotify:album:6fC6BRXbuHSVobwWcwe6M7',
-  'spotify:album:6yitEMT7G4qfIcV3jWXP8I',
-  'spotify:album:0gTbDhU0tDmXdXn2RNWO2l'
+  'spotify:album:0O4U8aVbBcWrq38ax0T6AO',     // The Emancipation Of Mimi (20th Anniversary Edition)
+  'spotify:album:0SHpIbyBLUugMXsl3yNkUz',     // Emotions
+  'spotify:album:0cS9prZ8u3fdbc7lmtCaMV',     // Merry Christmas (Deluxe Anniversary Edition)
+  'spotify:album:0v1DRRYBXYg1uVN1CIsyy0',     // The Rarities
+  'spotify:album:1GzBLltCCKsiIJM3T3AWj3',     // Music Box: 30th Anniversary Edition
+  'spotify:album:1ibYM4abQtSVQFQWvDSo4J',     // Daydream
+  'spotify:album:2VtWkFLhMJAFsWkHAXwosS',     // Caution (Japan Version)
+  'spotify:album:2hHFZLYnwsYOOxTCrlNvg0',     // Glitter
+  'spotify:album:2tVxdAJ16NY9bpjb1h5fQc',     // Me. I Am Mariah…The Elusive Chanteuse (Deluxe)
+  'spotify:album:31MluXLYC0ZnCSfUZ5T4GX',     // E=MC2 (Deluxe Version)
+  'spotify:album:3RPImDZ7Ihh5YR5iJh1gH1',     // Memoirs of an imperfect Angel
+  'spotify:album:3VOqo81Nwyx8rcZEc2l379',     // Butterfly: 25th Anniversary Expanded Edition
+  'spotify:album:6fC6BRXbuHSVobwWcwe6M7',     // Merry Christmas II You
+  'spotify:album:5SwNGsGw1I8H361DKiYnnn',     // Mariah Carey
+  'spotify:album:5VfesyhwiNgpEEPXlO5c84',     // Mariah Carey's Magical Christmas Special (Apple TV+ Original Soundtrack)
+  'spotify:album:6MljmKZLh52AUR1v5WpWst',     // Here For It All
+  'spotify:album:6aouVhqJ9SkEUS2gAR0xBS',     // Merry Christmas: 30th Anniversary Edition
+  'spotify:album:6yitEMT7G4qfIcV3jWXP8I',     // Rainbow: 25th Anniversary Expanded Edition
+  'spotify:album:0gTbDhU0tDmXdXn2RNWO2l'      // Charmbracelet
 ]
